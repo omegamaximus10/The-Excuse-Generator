d320d88059403a129a65cfddb14a4cade3a07174
omegamaximus10 <bryan_2904@hotmail.com>
2021-07-06 19:04:07 +0000

    ejercicio finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+
+  document.querySelector("#the-excuse").i;
   console.log("Hello Rigo from the console!");
 };
